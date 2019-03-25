36e9a731fd253e61af11c49c235be914363b2310
Josh Swan <joshuawswan@gmail.com>
2019-03-24 23:57:57 -0700

    Refactor exports to improve CommonJS compatibility

---
+++ src/Config.ts
@@ -0,0 +1,159 @@
+/*!
+ * konfiguration
+ *
+ * Copyright 2017-2019 Josh Swan
+ * Released under the MIT license
+ * https://github.com/joshswan/konfiguration/blob/master/LICENSE
+ */
+
+import fs from 'fs';
+import path from 'path';
+import dotenv from 'dotenv';
+import yaml from 'js-yaml';
+import { camelCase, get, set } from 'lodash';
+
+interface EnvVars {
+  [key: string]: any;
+}
+
+function convert(value: any, type: string): any {
+  switch (type) {
+    case 'boolean':
+      return /^(?:true|1)$/i.test(value);
+    case 'number':
+      return parseFloat(value);
+    case 'string':
+      return String(value);
+    default:
+      return value;
+  }
+}
+
+function isObject(value: any): boolean {
+  return typeof value === 'object' && !Array.isArray(value);
+}
+
+function reduce(destination: object, source: EnvVars): EnvVars {
+  // Reduce source object keys, using destination as initial value
+  return Object.keys(source).reduce((reduced: object, sourceKey: string) => {
+    // Create config key
+    const key = sourceKey
+      // Remove non-word characters
+      .replace(/\W/g, '')
+      // Split on double-underscore for nesting
+      .split('__')
+      // Remove empty values
+      .filter(Boolean)
+      // Convert keys to camel-case
+      .map(str => camelCase(str))
+      // Convert to single string with dot notation
+      .join('.');
+
+    const destValue = get(destination, key);
+    const sourceValue = source[sourceKey];
+
+    // If new value is an object, merge recursively, and use current value as destination if object
+    // Otherwise, set new value at key
+    if (isObject(sourceValue)) {
+      set(reduced, key, reduce(isObject(destValue) ? destValue : {}, sourceValue));
+    } else {
+      set(reduced, key, convert(sourceValue, typeof destValue));
+    }
+
+    return reduced;
+  }, destination);
+}
+
+class Config {
+  [key: string]: any;
+
+  public ENV!: string;
+  private dir!: string;
+  private prefix!: string;
+
+  constructor() {
+    // eslint-disable-next-line no-multi-assign
+    this.ENV = this.getEnv('NODE_ENV', 'development');
+    this.dir = this.getEnv('NODE_CONFIG_DIR', path.join(process.cwd(), 'config'));
+    this.prefix = this.getEnv('NODE_CONFIG_PREFIX', '').toUpperCase();
+
+    this.loadFiles();
+    this.loadEnv();
+  }
+
+  environment(...envs: string[]): boolean {
+    return envs.includes(this.ENV);
+  }
+
+  get(key: string, defaultValue?: any): any {
+    return get(this, key, defaultValue);
+  }
+
+  set(key: string, value: any): void {
+    set(this, key, value);
+  }
+
+  private getEnv(key: string, defaultValue?: any): any {
+    const value = process.env[key] || defaultValue;
+
+    this[key] = value;
+
+    return value;
+  }
+
+  private loadFiles(): void {
+    // Create list of *.yml, *.ENV.yml, and ENV/*.yml files
+    const files = ['', this.ENV].map((subdir) => {
+      const dir = path.join(this.dir, subdir);
+      const suffix = `.${this.ENV}.`;
+
+      if (!fs.existsSync(dir)) return [];
+
+      return fs.readdirSync(dir)
+        .filter(file => /\.(yml|yaml)$/.test(file))
+        .filter(file => !/\w*\.\w*\./.test(file) || file.includes(suffix))
+        .sort((a, b) => {
+          if (!a.includes(suffix) && b.includes(suffix)) return -1;
+          if (a.includes(suffix) && !b.includes(suffix)) return 1;
+          return 0;
+        })
+        .map(file => path.resolve(dir, file));
+    }).reduce((a, b) => a.concat(b));
+
+    // Parse files and merge into config
+    Object.assign(this, files.map(file => yaml.load(fs.readFileSync(file, 'utf8'))).filter(Boolean).reduce(reduce, {}));
+  }
+
+  private loadEnv(): void {
+    dotenv.config();
+
+    let env = {};
+
+    if (this.getEnv('NODE_CONFIG')) {
+      try {
+        Object.assign(env, JSON.parse(this.getEnv('NODE_CONFIG')));
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.warn('NODE_CONFIG environment variable is invalid JSON');
+      }
+    }
+
+    env = Object.keys(process.env)
+      // Filter environment variables by prefix
+      .filter((key: string) => (
+        key.toUpperCase().startsWith(this.prefix)
+      ))
+      // Create environment variable object with filtered keys
+      .reduce((vars: EnvVars, key: string) => {
+        vars[key.substring(this.prefix.length)] = process.env[key];
+
+        return vars;
+      }, env);
+
+    // Merge environment variables into config
+    reduce(this, env);
+  }
+}
+
+export { Config };
+export default Config;
---
+++ src/__tests__/Config.test.ts
@@ -0,0 +1,199 @@
+/*!
+ * konfiguration
+ *
+ * Copyright 2017-2019 Josh Swan
+ * Released under the MIT license
+ * https://github.com/joshswan/konfiguration/blob/master/LICENSE
+ */
+
+import path from 'path';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Config } from '../Config';
+
+beforeEach(() => {
+  process.env = {
+    NODE_ENV: 'test',
+  };
+});
+
+describe('Config', () => {
+  it('should parse files in NODE_CONFIG_DIR directory', () => {
+    process.env.NODE_CONFIG_DIR = path.resolve(__dirname, '../../test/config');
+
+    const config = new Config();
+
+    expect(config.get('app.test')).to.be.true;
+    expect(config.get('app.port')).to.equal(3000);
+    expect(config.get('app.version')).to.equal('1.0.0');
+    expect(config.get('database')).to.eql({
+      username: 'test',
+      port: 1234,
+    });
+  });
+
+  it('should merge files with *.ENV.yml names', () => {
+    process.env.NODE_CONFIG_DIR = path.resolve(__dirname, '../../test/config-env');
+
+    const test = new Config();
+    expect(test.get('app.test')).to.equal('test');
+
+    process.env.NODE_ENV = 'development';
+
+    const dev = new Config();
+    expect(dev.get('app.test')).to.equal('default');
+
+    process.env.NODE_ENV = 'production';
+
+    const prod = new Config();
+    expect(prod.get('app.test')).to.equal('production');
+  });
+
+  it('should merge files with ENV/*.yml names', () => {
+    process.env.NODE_CONFIG_DIR = path.resolve(__dirname, '../../test/config-env-dir');
+
+    const test = new Config();
+    expect(test.get('app.test')).to.equal('test');
+
+    process.env.NODE_ENV = 'development';
+
+    const dev = new Config();
+    expect(dev.get('app.test')).to.equal('default');
+
+    process.env.NODE_ENV = 'production';
+
+    const prod = new Config();
+    expect(prod.get('app.test')).to.equal('production');
+  });
+
+  it('should merge environment variables', () => {
+    process.env.NODE_CONFIG_DIR = path.resolve(__dirname, '../../test/config');
+    process.env.APP__TEST = '0';
+    process.env.APP__PORT = '8000';
+    process.env.DATABASE__USERNAME = 'prod';
+
+    const config = new Config();
+
+    expect(config.get('app.test')).to.be.false;
+    expect(config.get('app.port')).to.equal(8000);
+    expect(config.get('app.version')).to.equal('1.0.0');
+    expect(config.get('database')).to.eql({
+      username: 'prod',
+      port: 1234,
+    });
+  });
+
+  describe('NODE_CONFIG', () => {
+    it('should parse and merge JSON string from NODE_CONFIG environment variable', () => {
+      process.env.NODE_CONFIG_DIR = path.resolve(__dirname, '../../test/config');
+      process.env.NODE_CONFIG = JSON.stringify({ app: { version: '1.0.0-1' }, database: { username: 'prod' } });
+
+      const config = new Config();
+
+      expect(config.get('app.test')).to.be.true;
+      expect(config.get('app.port')).to.equal(3000);
+      expect(config.get('app.version')).to.equal('1.0.0-1');
+      expect(config.get('database')).to.eql({
+        username: 'prod',
+        port: 1234,
+      });
+    });
+
+    it('should warn on invalid JSON in NODE_CONFIG environment variable', () => {
+      process.env.NODE_CONFIG = '{ invalid_json = 1';
+
+      const warn = sinon.stub(console, 'warn');
+      const config = new Config();
+
+      expect(warn).to.be.calledOnce;
+      expect(warn).to.be.calledWith('NODE_CONFIG environment variable is invalid JSON');
+      expect(config).to.be.instanceOf(Config);
+
+      warn.restore();
+    });
+  });
+
+  describe('NODE_CONFIG_PREFIX', () => {
+    it('should merge environment variables prefixed with NODE_CONFIG_PREFIX', () => {
+      process.env.NODE_CONFIG_DIR = path.resolve(__dirname, '../../test/config');
+      process.env.NODE_CONFIG_PREFIX = 'APP_';
+      process.env.APP_DATABASE__USERNAME = 'prod';
+      process.env.APP_APP__PORT = '80';
+
+      const config = new Config();
+
+      expect(config.get('app.test')).to.be.true;
+      expect(config.get('app.port')).to.equal(80);
+      expect(config.get('app.version')).to.equal('1.0.0');
+      expect(config.get('database')).to.eql({
+        username: 'prod',
+        port: 1234,
+      });
+    });
+
+    it('should not merge unprefixed environment variables', () => {
+      process.env.NODE_CONFIG_DIR = path.resolve(__dirname, '../../test/config');
+      process.env.NODE_CONFIG_PREFIX = 'APP_';
+      process.env.DATABASE__USERNAME = 'prod';
+
+      const config = new Config();
+
+      expect(config.get('database')).to.eql({
+        username: 'test',
+        port: 1234,
+      });
+    });
+  });
+
+  describe('environment()', () => {
+    it('should test if arguments match current environment', () => {
+      const config = new Config();
+
+      expect(config.environment('test')).to.be.true;
+      expect(config.environment('development', 'staging', 'test')).to.be.true;
+      expect(config.environment('production')).to.be.false;
+      expect(config.environment('production', 'development')).to.be.false;
+    });
+  });
+
+  describe('get()', () => {
+    it('should return config value at specified key', () => {
+      const config = new Config();
+      config.test = 1;
+
+      expect(config.get('test')).to.equal(1);
+    });
+
+    it('should accept dot notation to return nested keys', () => {
+      const config = new Config();
+      config.test = { enabled: 1 };
+
+      expect(config.get('test.enabled')).to.equal(1);
+    });
+
+    it('should return supplied default value if key does not exist', () => {
+      const config = new Config();
+
+      expect(config.get('key.does.not.exist', 'defaultVal')).to.equal('defaultVal');
+    });
+  });
+
+  describe('set()', () => {
+    it('should set config value at specified key', () => {
+      const config = new Config();
+      config.set('test', 1);
+
+      expect(config.test).to.equal(1);
+      expect(config.get('test')).to.equal(1);
+    });
+
+    it('should accept dot notation for nested keys', () => {
+      const config = new Config();
+      config.test = { enabled: 1 };
+      config.set('test.enabled', 0);
+
+      expect(config.test.enabled).to.equal(0);
+      expect(config.get('test.enabled')).to.equal(0);
+    });
+  });
+});
